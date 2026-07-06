454755c6478062843066c2c462885b7167713ec6
Zachary Gray <zacharytgray@gmail.com>
2026-07-06 11:58:28 -0500

    Update HyprMac to v0.8.0

--- Casks/hyprmac.rb
+++ Casks/hyprmac.rb
@@ -1,6 +1,6 @@
 cask "hyprmac" do
-  version "0.7.0"
-  sha256 "919319078ef2034202624c74743e57dd122be8d5e9f360e6d6cf45c731b106eb"
+  version "0.8.0"
+  sha256 "ba3b70ad8adee09b622ae575e782a25083c9f0af68aa128141a0274e6445ff66"
 
   url "https://github.com/zacharytgray/HyprMac/releases/download/v#{version}/HyprMac-#{version}.dmg"
   name "HyprMac"
